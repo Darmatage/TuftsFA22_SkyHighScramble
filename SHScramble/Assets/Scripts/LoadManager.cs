845772cfa4deed77badfa60c14a0526d066c5eb4
whe05 <wanming.he@tufts.edu>
2022-10-08 23:26:07 -0400

    Add a load screen between title page and clouds scene

---
+++ SHScramble/Assets/Scripts/LoadManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadManager : MonoBehaviour
+{
+    public GameObject loadScreen;
+    public Slider slider;
+    public Text text;
+
+    public void LoadNextLevel() {
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel() {
+        //loadScreen.setActive(true);
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync("Clouds");
+
+        operation.allowSceneActivation = false;
+         while (!operation.isDone)
+        {
+            slider.value = operation.progress;
+            //Output the current progress
+            text.text = "Loading progress: " + (operation.progress * 100) + "%";
+
+            // Check if the load has finished
+            if (operation.progress >= 0.9f)
+            {
+                slider.value = 1;
+                //Change the Text to show the Scene is ready
+                text.text = "Press the space bar to continue";
+                //Wait to you press the space key to activate the Scene
+                if (Input.GetKeyDown(KeyCode.Space))
+                    //Activate the Scene
+                    operation.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+}
