b0b261fbc76a416c195ab00d5376fdf448f7c257
whe05 <wanming.he@tufts.edu>
2022-10-08 20:23:08 -0400

    Add a start page
    The function is really simple right now and will be developed more.
    The background picture is temporary and will need another one

---
+++ SHScramble/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play() {
+        SceneManager.LoadScene("Clouds");
+    }
+
+    public void QuitGame() {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
